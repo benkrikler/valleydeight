13715e5700c7ab50c6b7aecbf803f72a6e70ef14
Ben Krikler <bek07@ic.ac.uk>
2018-06-13 00:45:12 +0200

    Add more package tags

--- setup.py
+++ setup.py
@@ -19,5 +19,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
     ),
 )
