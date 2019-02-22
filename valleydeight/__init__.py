5640aced1ba7296ef90a85f48c102df548e2d4f0
Ben Krikler <bek07@ic.ac.uk>
2019-02-22 17:04:57 +0100

    Set up for bumpversion

--- valleydeight/__init__.py
+++ valleydeight/__init__.py
@@ -5,3 +5,7 @@ from .simple_types import Str, Int, Bool, Float
 from .choice_types import Choice
 from .object_types import Object
 from .base import Pass
+
+
+__author__ = "Ben Krikler"
+__version__ = '0.0.2'
